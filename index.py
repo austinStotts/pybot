0832b8663a8b5ee4051e0d538fea604267f74e96
Austin Stotts <austintstotts@gmail.com>
2021-01-08 09:20:38 -0600

    added how to

--- index.py
+++ index.py
@@ -7,11 +7,31 @@ from pynput.keyboard import Controller as KController
 from threading import Timer
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+# * * * * * * * * * * * * * * * *
+# PLEASE READ
+# 
+# you will need to install some python packages before this script will work:
+# 1. pynput -> use "pip install pynput"
+# 2. apscheduler -> use "pip install apscheduler"
+#
+# once these are installed you can open a commandprompt window
+# and navigate into the project folder -> use "cd Desktop/pybot"
+# 
+# type "python index.py" to run the program
+# this program has no means to stop itself and will continue to run untill you close the window
+#
+# if you wish to change the cordinates for this program you can use "python findMouseCords.py"
+# type the command into commandprompt then put your mouse where you want then press enter
+#
+# * * * * * * * * * * * * * * * * *
+
+
 m = MController()
 k = KController()
 
-hours = 2 # time for kakera claim to reset
-command = "$mk" # mudae command to use
+# SET THESE VARIABLES BEFORE RUNNING PROGRAM
+hours = 2 # time interval
+command = "$mk" # what to be typed
 
 
 def collect_mk():
@@ -32,9 +52,6 @@ def collect_mk():
   # wait for reaction to apear and click
   time.sleep(2)
   m.click(Button.left, 1)
-
-
-
 
 # instantiate scheduler and start the timer
 scheduler = BlockingScheduler()
